e5aa82dcb332eac6184779f0ee2991913a10d642
Deepanjali <deepanjali20@navgurukul.org>
2024-02-24 00:15:16 +0530

    header complete

--- app/header.js
+++ app/header.js
@@ -1,12 +1,9 @@
 const Header = () => {
     return(
-        <div className="header" >
-            <div className="headertext" >NewsAPI</div>
-            <div><img src="Ellipse 1.png"/> </div>
-            <div className="headertext" >Org</div>
-                           
+        <div className="container">
+            <div className="header" >NewsAPI<span ><img className="image-header"src="Ellipse 1.png"/></span>Org </div>
+            <div className="hr" ></div>
         </div>
-        
     );    
 };
 export default Header;
--- app/page.js
+++ app/page.js
@@ -1,1 +1,11 @@
 "use client";
+import Header from "./header.js";
+import 'bootstrap/dist/css/bootstrap.css'
+
+export default function Home() {
+  return (
+    <div class="container-fluid" >  
+      <Header/>
+      </div>
+  )
+}
