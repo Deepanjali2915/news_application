559c40f709a17057db33955af6289399a4f8933d
Deepanjali <deepanjali20@navgurukul.org>
2024-02-24 11:41:25 +0530

    search bar complete

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 "use client";
 import Header from "./header.js";
+import Search from "./search.js";
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function Home() {
   return (
     <div class="container-fluid" >  
       <Header/>
+      <Search/>
       </div>
   )
 }
---
+++ app/search.js
@@ -0,0 +1,12 @@
+const Search = () => {
+    return (
+        <div className="input-group">
+            <div className="form-outline">
+                <div><img src="Frame 15.png"/></div>
+                <input type="search" className="search" placeholder="Search" />    
+            </div>
+            <button className="form-label" >GO</button>
+        </div>
+    );
+};
+export default Search;
